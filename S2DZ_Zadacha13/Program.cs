e5cd60778135945dcc12d631fd0e5bab4752541a
AAVinogradov <aavinogradov@bk.ru>
2023-06-18 23:06:51 +0300

    составил задачу 13 по дз семинара 2

---
+++ S2DZ_Zadacha13/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Введите число число не больше 2147483647: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (N >= 100 && N <= 999)
+{
+int Midle = (N / 1) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 1000 && N <= 9999)
+{
+int Midle = (N / 10) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 10000 && N <= 99999)
+{
+int Midle = (N / 100) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 100000 && N <= 999999)
+{
+int Midle = (N / 1000) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 1000000 && N <= 9999999)
+{
+int Midle = (N / 10000) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 1000000 && N <= 9999999)
+{
+int Midle = (N / 100000) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 10000000 && N <= 99999999)
+{
+int Midle = (N / 100000) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 100000000 && N <= 999999999)
+{
+int Midle = (N / 1000000) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
+else if (N >= 1000000000 && N <= 2147483647)
+{
+int Midle = (N / 10000000) % 10; 
+Console.Write("Третья цифра: ");
+Console.WriteLine(Midle);
+}
