4fbd4f55ba6dabc56e2c2b1fe660d67d7d53ad34
AAVinogradov <aavinogradov@bk.ru>
2023-06-18 18:43:42 +0300

    дополнил задачу 2 семенира 3, добавил округление и исправил ошибки

--- S3Zadacha2/Program.cs
+++ S3Zadacha2/Program.cs
@@ -3,8 +3,12 @@
 // A (3,6); B (2,1) -> 5,09 - не корректно
 // A (7,-5); B (1,-1) -> 7,21
 
+
+// Math.Sqrt(x) - корень
+// Math.pow(x, 2) квадрат
+
 Console.WriteLine("Введите X первой точки");
-int x1 = Convert.ToInt32(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());   
 Console.WriteLine("Введите Y первой точки");
 int y1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите X второй точки");
@@ -17,4 +21,4 @@ double skobY = Math.Pow(y1 - y2, 2);
 
 double result = Math.Sqrt(skobX + skobY);
 
-Console.WriteLine(result);
+Console.WriteLine($"{result:f2}"); // 2 - число знаков которые нужно округлить, например 7,21 - интерполяция
