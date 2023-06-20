aa059c6198e1a4d6c22e51f7e431455bdb12b18f
AAVinogradov <aavinogradov@bk.ru>
2023-06-20 21:38:03 +0300

    составил задачу 28 семинара 4 - работа с функциями

---
+++ S4Zadacha28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt()
+{
+    Console.WriteLine("Введите целое число");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+
+}
+
+int Summ(int number)
+{
+    int result = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        result *=i;
+    }
+    return result;
+}
+
+int number = ReadInt();
+int Summa = (Summ(number));
+
+Console.WriteLine ($"Факториал числа {number} равен {Summa}");
