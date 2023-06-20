347f768c760b106919735a8c02cd7f52b16c502b
AAVinogradov <aavinogradov@bk.ru>
2023-06-20 20:43:43 +0300

    составил задачу 24 семинара 4 - работа с функциями

---
+++ S4Zadacha24/Program.cs
@@ -0,0 +1,35 @@
+﻿// Демонстрация решения
+// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ReadInt()
+{
+    Console.WriteLine("Введите целое число");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumFrom1ToA(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum = sum + i;
+        //sum += i;
+    }
+    return sum;
+}
+
+// int value1 = ReadInt();
+// int value2 = ReadInt();
+// int value3 = ReadInt();
+// int value4 = ReadInt();
+// int value5 = ReadInt();
+// int value6 = ReadInt();
+
+int number = ReadInt();
+int summa = SumFrom1ToA(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {summa}");
+
