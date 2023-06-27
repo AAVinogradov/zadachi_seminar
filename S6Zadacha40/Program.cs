7ef615f25ac2dcc4a4cadf613d0db713f3cec617
AAVinogradov <aavinogradov@bk.ru>
2023-06-27 22:09:01 +0300

    составил задачу 40  семинара 6 - стороны треугольника

---
+++ S6Zadacha40/Program.cs
@@ -0,0 +1,18 @@
+﻿// адача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Вееди сторону А ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вееди сторону B ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вееди сторону C ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A + B > C && A + C > B && B + C > A)
+{
+    Console.WriteLine("Да может");
+}
+else
+{
+    Console.WriteLine("Нет, не может");
+}
