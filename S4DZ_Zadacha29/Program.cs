84f1f782af301ac7ded4f1c749c02895ce54c3b6
AAVinogradov <aavinogradov@bk.ru>
2023-07-02 22:23:36 +0300

    составил задачу ДЗ 29 семинара 4 - ввод с клавиатуры размера и элементов массива  и его вывод

---
+++ S4DZ_Zadacha29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+Console.Write("Введите длмнну масива: ");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[elementsCount];
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент масива под индексом {i}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива: ");
+for(int i = 0; i < elementsCount; i++)
+    {
+    Console.WriteLine(array [i]);
+    }
+
+        
