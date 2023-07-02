6d9afff468a1dbf62f5c878e44afbf427ca375df
AAVinogradov <aavinogradov@bk.ru>
2023-07-02 17:42:12 +0300

    составил задачу ДЗ 25 семинара 4 - возведение в степень

---
+++ S4DZ_Zadacha24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Exponentiation(int num, int deg)
+{
+int number = 1;
+    for (int i = 1; i <= deg; i++)
+    {
+        number = number * num;
+       
+    }
+    return number;
+}
+
+int number = ReadInt();
+int degree = ReadInt();
+int result = Exponentiation(number, degree);
+Console.WriteLine($"Число {number} в степени {degree} = {result}");
