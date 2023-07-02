7abdcb44daf2f002c5f9c4d2e081602f0aa56738
AAVinogradov <aavinogradov@bk.ru>
2023-07-02 18:45:18 +0300

    составил задачу ДЗ 27 семинара 4 - сумма чисел в цифре

---
+++ S4DZ_Zadacha27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SummaAllDigital(int digital)
+{
+    int result = 0;
+    while (digital > 0)
+    {
+        result = result + digital % 10;
+        digital = digital / 10;
+    }
+    return result;
+}
+
+int digital = ReadInt();
+int SumDigital = SummaAllDigital(digital);
+Console.WriteLine($"Сумма чисел цифры {digital} будет равна - {SumDigital}");
