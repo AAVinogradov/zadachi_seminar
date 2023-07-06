bfb33b5cbcde976e1b6bb13d05584a0fc5dc2fca
AAVinogradov <aavinogradov@bk.ru>
2023-07-06 21:34:12 +0300

    составил задачу ДЗ 36 семинара 5 - сумма чисел стоящих на нечетных позицияхл в массиве

---
+++ S5DZ_Zadacha36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetRandomArray()
+{
+    int[] array = new int[10];
+for(int i = 0; i < array.Length; i++)
+    {
+    array[i] = Random.Shared.Next(-10, 11);
+    }
+    return array;
+}
+
+int SumOfOddElements(int[] massive)
+{
+    int oddElements = 0;
+    for(int i = 1; i < massive.Length; i+=2)
+    {
+        oddElements = massive[i] + oddElements;
+    }
+    return oddElements;
+}
+
+int[] myArray = GetRandomArray();
+int sumOddElements = SumOfOddElements(myArray);
+
+Console.Write("Сгенерированный массив:");
+Console.WriteLine($"[{string.Join(" ", myArray)}]");
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях {string.Join(", ", myArray)} = {sumOddElements}");
