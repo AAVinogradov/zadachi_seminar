1509f03d31f3e04b61f4119db02a1e5c917d4ddc
AAVinogradov <aavinogradov@bk.ru>
2023-07-21 00:02:41 +0300

    составил задачу ДЗ 64 семинара 9 - вывод значений промежутка от большего к меньшему с помощью рекурсии

---
+++ S9DZ_Zadacha64/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите значение N: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение точки отсчета: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (x > number)
+{
+    Console.Write("Не верно задан промежуток ");  
+}
+else
+{
+
+void Enumeration(int FirstNumber, int NumberN )
+{
+    if (FirstNumber > NumberN) return;
+    Enumeration(FirstNumber + 1, NumberN);
+    Console.Write (FirstNumber + "   " );
+
+}
+      
+
+Enumeration(x, number);
+
+}
+
+
+
