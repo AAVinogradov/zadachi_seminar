0ff8f23d7a020e7bd02281afb028c6bc34b7d584
AAVinogradov <aavinogradov@bk.ru>
2023-07-23 15:19:41 +0300

    составил задачу ДЗ 66 семинара 9 - сумма чисел в провежуткеу с помощью рекурсии

---
+++ S9DZ_Zadacha66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n)
+{
+    Console.Write("Не верно задан промежуток ");  
+}
+
+else
+{
+
+int NaturalNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN) return 0;
+    return numberM + NaturalNumbers(numberM + 1, numberN);
+}
+    
+
+// NaturalNumbers(m, n);
+Console.WriteLine(NaturalNumbers(m, n));
+}
